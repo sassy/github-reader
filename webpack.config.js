e6f2a7f66129520dcf544c2b6cc54204120bf82a
Satoshi Watanabe <sassy.watanabe@gmail.com>
2016-10-23 08:52:28 +0900

    use typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  entry: './renderer/js/index.js',
+  entry: './renderer/js/index.ts',
   output: {
     filename: './renderer/bundle.js'
   },
+  resolve: {
+    extensions: ['', '.webpack.js', 'web.js', '.ts', '.tsx', '.js']
+  },
   module: {
     loaders: [
-      { test: /\.json$/, loader: 'json-loader' }
+      { test: /\.json$/, loader: 'ts-loader' }
     ]
   }
 };
